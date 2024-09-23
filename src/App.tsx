b38d18002a4774ac4de33df97e907470fbf4daec
Randell Lapid <randelljustinlapid@gmail.com>
2024-09-23 02:04:47 -0700

    added favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { useEffect } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
 function App() {
+  // useEffect(() => {
+  //   document.title = "Gacha Savior";
+  // })
   return (
     <div>
       <Header />
