518771dad49390ce5f9648b55289e94b3ee363b5
Randell Lapid <randelljustinlapid@gmail.com>
2024-09-23 02:05:39 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
