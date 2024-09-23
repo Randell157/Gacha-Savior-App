2d511eaed9864cc8f5ba7b23a7f4f49cab8ace08
Randell Lapid <randelljustinlapid@gmail.com>
2024-09-23 01:34:47 -0700

    Practice vite+react+ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-import Message from "./Message"
-function App(){
-  return <div><Message></Message></div>
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  return (
+    <div>
+      <Button color="primary" onClick={() => console.log("Clicked")}>MyButton</Button>
+    </div>
+  );
 }
 
 export default App;
