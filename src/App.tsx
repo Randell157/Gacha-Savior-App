598b8f7811b453c8cd788c3dd1e70cbd8aa3fa06
Randell Lapid <randelljustinlapid@gmail.com>
2024-09-23 01:53:33 -0700

    fixed header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Footer from "./components/Footer";
-import Header from "./components/header";
+import Header from "./components/Header";
 import Main from "./components/Main";
 
 function App() {
